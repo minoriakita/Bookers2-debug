90db3b5235ad906ad288086a1b2c180adffcfd44
minoriakita <minori.akita@gmail.com>
2022-04-26 07:13:06 +0000

    [Update] 応用課題４　フォロー機能途中

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   resources :book_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
 end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+  resource :relationships, only: [:create, :destroy]
+
+  get 'relationships/followings', as: 'followings'
+  get 'relationships/followers', as: 'followers'
+end
 
 end
